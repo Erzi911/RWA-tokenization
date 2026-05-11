b248a69cc788622f3784228ceaef94a0e7b3cfd6
Yerassyl <marshall2508l@gmail.com>
2026-05-11 08:58:43 +0500

    fixed adapter

--- test/ChainlinkAdapter.t.sol
+++ test/ChainlinkAdapter.t.sol
@@ -14,34 +14,33 @@ contract ChainlinkAdapterTest is BaseTest {
         assertEq(adapter.stalenessThreshold(), STALENESS);
     }
 
-    function test_revertsOnZeroFeed() public view {
+    function test_revertsOnZeroFeed() public {
         vm.expectRevert(ChainlinkAdapter.ZeroAddress.selector);
         new ChainlinkAdapter(address(0), STALENESS);
     }
 
-    function test_revertsOnZeroThreshold() public view {
+    function test_revertsOnZeroThreshold() public {
         vm.expectRevert(ChainlinkAdapter.InvalidThreshold.selector);
         new ChainlinkAdapter(address(mockFeed), 0);
     }
 
     function test_scales8DecTo18Dec() public view {
-        // feed returns 1_000e8, should get back 1_000e18
         assertEq(adapter.latestPrice(), 1_000e18);
     }
 
-    function test_scales18DecFeed() public view {
+    function test_scales18DecFeed() public {
         MockAggregator f = new MockAggregator(18, 2_000e18);
         ChainlinkAdapter a = new ChainlinkAdapter(address(f), STALENESS);
         assertEq(a.latestPrice(), 2_000e18);
     }
 
-    function test_scales6DecFeed() public view {
+    function test_scales6DecFeed() public {
         MockAggregator f = new MockAggregator(6, 500e6);
         ChainlinkAdapter a = new ChainlinkAdapter(address(f), STALENESS);
         assertEq(a.latestPrice(), 500e18);
     }
 
-    function test_reverts_stalePrice() public view {
+    function test_reverts_stalePrice() public {
         mockFeed.makeStale(STALENESS + 1);
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -53,18 +52,18 @@ contract ChainlinkAdapterTest is BaseTest {
         adapter.latestPrice();
     }
 
-    function test_passes_freshPrice() public view {
+    function test_passes_freshPrice() public {
         mockFeed.makeStale(STALENESS - 1);
         assertGt(adapter.latestPrice(), 0);
     }
 
-    function test_reverts_negativePrice() public view {
+    function test_reverts_negativePrice() public {
         mockFeed.setAnswer(-1);
         vm.expectRevert(abi.encodeWithSelector(ChainlinkAdapter.NegativePrice.selector, int256(-1)));
         adapter.latestPrice();
     }
 
-    function test_reverts_zeroPrice() public view {
+    function test_reverts_zeroPrice() public {
         mockFeed.setAnswer(0);
         vm.expectRevert(ChainlinkAdapter.ZeroPrice.selector);
         adapter.latestPrice();
